3b9b48236c70e46297a6174c7209eca107d85af9
h f <waltermelonson@gmail.com>
2019-05-04 21:49:07 -0700

    Hopefully final of first phase, established circular functions allowing the continuous use of program if wanted, and removed all bugs from previous versions.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,23 @@
+//importing modules and declaring a variable used for spacing logs, as well as a couple variables to
+//overcome functional scopes. I need to look up and learn a better way to solve this problem.
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var spacer = '========================================================';
 var choice;
 var stock;
 
+//establishing a connection to my hosted connection for my database of mySQL workbench
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     port: 3306,
-    password: '1800Petmeds?',
+    password: '123',
     database: 'bamazon'
 });
 
+//Once the connection is established a query is initiated and within it a welcome message is displayed
+//(a little too big of one), and the only explicitly called function that will be called, as all
+//other are then called within it
 connection.connect(function (err) {
     if (err) {
         console.error('Error connecting: ' + err.stack);
@@ -46,89 +52,168 @@ connection.connect(function (err) {
     #  ▄█████████▀!!!!███!!!!█▀!!!!▀█!!!███!!!█▀!!!!███!!!!█▀!!!▀████████▀!!▀██████▀!!!▀█!!!█▀!!
     #  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
     startUp();
-
     
+})
+
+//A checkpoint function that is called at the end of all others to prompt the user if they want to exit,
+//or begin the process over.
+function continueShop(){
+    inquirer.prompt([{
+        type: 'confirm',
+        name: 'continue',
+        message: 'Would you like to continue shopping?'
+    }])
+    .then(function(res){
+        switch(res.continue){
+            case true:   
+            displayItems();
+            break;
+            
+            case false:
+            exit();
+            break;
+        }
+    })
+}
+
+//This function after the query has populated the list of items in the database, prompts the user 
+//for a choice of item to purchase.
+function pickItem() {
     inquirer.prompt([{
             type: "input",
             name: "start",
             message: "Please enter the ID of the item you wish to purchase."
         }])
         .then(function (ans) {
-            console.log('yes');
+
+            //Placing the users choice in a variable first declared globally, its placed into another query
+            //to grab specified values for use later.
             choice = ans.start;
             connection.query('SELECT * FROM products WHERE item_id = ? ORDER BY item_id', choice, function (err, results) {
+                
                 if (err) {
                     console.log(err);
                     return;
-                } else if (results) {
+                }
+
+                //A validation check, if they choose a number not in the database, it restarts,
+                //prompting again.
+                else if(choice > 10 || choice < 1){
                     
+                console.log(spacer);
+                console.log("No item exists with the Item ID!\n");
+                console.log("Please enter another Item ID");
+                console.log(spacer);
+                pickItem();
+
+                //Here the values that were grabbed are placed into the variables and the price
+                //is calculated and pushed into a callback function.
+                }else if (results) {
                     stock = results[0].stock_quantity;
                     var price = results[0].price;
-                    console.log(results);
                     buy(price, stock);
-                    
-                    //exit();
+                
+                //If somehow neither of these requirements are meant the program exits.
                 } else {
                     exit();
                 }
             })
-
-
         })
+}
 
-    //exit();
-})
-
-function buy(price, stock){
+//Here is the callback function which passes the arguments in, and upon recieving a response from
+//the following prompt, calcutes the price and passes it the user, calling the continueShop function
+//once again, asking if to exit, or restart.
+function buy(price, stock) {
     inquirer.prompt([{
-        type: 'input',
-        name:'amount',
-        message: 'How many would you like to purchase?'
-    }])
-    .then(function(res){
-        var amount = res.amount;
-        if(res.amount > stock){
-            console.log("You want more than we have available!\n");
-            console.log("Please enter another amount.");
-            buy();
-        }
-        else{
-            var cost = res.amount * price;
-            var updated = stock - amount;
-            // console.log(res.amount);
-            // console.log(thisThing);
-            console.log("You're total cost is: $" + cost);
-            deduct(updated, choice);
-            exit();
-        }
+            type: 'input',
+            name: 'amount',
+            message: 'How many would you like to purchase?'
+        }])
+        .then(function (res) {
+            
+            //Before the cost is calculated, the amount is tested against a conditional
+            //to make sure it is not above the stock_quantity, if so, alerts such to the user,
+            //and prompts the user again for a number that fulfills the limit. 
+            var amount = res.amount;
+            if (res.amount > stock) {
+                console.log(spacer);
+                console.log("You want more than we have available!\n");
+                console.log("Please enter another amount.");
+                console.log(spacer);
+                buy(price, stock);
+            } else {
+                var cost = res.amount * price;
+                var updated = stock - amount;
+                console.log(spacer);
+                console.log("You're total cost is: $" + cost);
+                deduct(updated, choice);
+                console.log("We're down to " + updated + " of that product!");
+                console.log(spacer);
+                continueShop();
+            }
+        })
+}
+
+//Quick small callback function of a query to update the database with the new amount following the sale.
+function deduct(updated, choice) {
+    connection.query('UPDATE products SET stock_quantity = ' + updated + ' WHERE item_id = ?', choice, function (res) {
     })
 }
 
-function deduct(updated, choice){
-    connection.query('UPDATE products SET stock_quantity = '+ updated +' WHERE item_id = ?', choice, function(res){
-        console.log("We're down to " + updated + " of that product!");
-    } 
-    )
-}
+//The initial function used to populate the screen after querying for all the available products in the
+//database.
+function displayItems(){
 
-function startUp() {
-    console.log('Hello and welcome!');
+    console.log('Here is a list of the available products!');
     connection.query('SELECT * FROM products', function (err, results, fields) {
         if (err) {
             console.log(err);
         }
         for (var i = 0; i < results.length; i++) {
             console.log(spacer);
-            console.log('Item #: ' + results[i].item_id + '\nProduct: ' + results[i].product_name +
-                '\nDepartment: ' + results[i].department_name + '\nPrice: ' + results[i].price);
+            console.log('Item #: ' + results[i].item_id + 
+                '\nProduct: ' + results[i].product_name +
+                '\nDepartment: ' + results[i].department_name + 
+                '\nPrice: ' + results[i].price + 
+                "\nStock: "+ results[i].stock_quantity);
             console.log(spacer);
         }
-        //console.log(results)
-    })
+        pickItem();
+    });
+    
 }
 
+//The first function called, simply asking the user if they wish to continue or exit the program, and
+//if they continue, begins a domino effect, calling other functions along the way.
+function startUp() {
+    inquirer.prompt([{
+            type: "list",
+            name: 'begin',
+            message: 'Would you like to purchase an item?',
+            choices: ["Yes", "No! I'll be on my way"]
+        }])
+        .then(function (res) {
+            switch (res.begin) {
+
+                case "Yes":
+                    displayItems();
+                    
+                    break;
+
+                case "No! I'll be on my way":
+                    exit();
+                    break;
+
+            }
+        })
+}
+
+//The simple exit function called throughtout the program, if called ends the connection, and displays
+//a message telling the user the program is ended.
 function exit() {
     connection.end(function (err) {
+        console.log(spacer);
         console.log('Thank you for shopping with us\r\n');
         console.log(`
         #                    8888888b.  888      8888888888        d8888  .d8888b.  8888888888                  
@@ -164,5 +249,5 @@ function exit() {
         #                                                                                                       
         #                                                                                                       
         #                                                                                                       `);
-    })
+    });
 };
